07b159908e4e8fb3650849ed241a6707cda6abbb
Calamity025 <vp5gor@gmail.com>
2019-05-21 22:57:39 +0300

    initial commit

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,237 @@
+﻿using System;
+
+namespace Practice1
+{
+    public class Matrix
+    {
+        public int[,] MatrixValue { get; private set; }
+
+        public Matrix()
+        {
+            MatrixValue = new int[0,0];
+        }
+
+        public Matrix(int[,] intMatrix)
+        {
+            if (intMatrix != null)
+            {
+                MatrixValue = intMatrix;
+            }
+            else
+            {
+                throw new ArgumentNullException("Matrix cannot be null");
+            }
+        }
+
+        public Matrix(Matrix matrix)
+        {
+            if (matrix != null)
+            {
+                MatrixValue = matrix.MatrixValue;
+            }
+            else
+            {
+                throw new ArgumentNullException("Matrix cannot be null");
+            }
+        }
+        public Matrix(int x, int y)
+        {
+            if (x > 0 && y > 0)
+            {
+                MatrixValue = new int[x, y];
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException
+                    ("Number of columns and rows cannot be lower or equals to 0");
+            }
+        }
+
+        public int this[int x, int y]
+        {
+            get => MatrixValue[x, y];
+            set => MatrixValue[x, y] = value;
+        }
+
+        public static Matrix operator *(Matrix a, int n)
+        {
+            if (a != null)
+            {
+                int xLength = a.MatrixValue.GetLength(0);
+                int yLength = a.MatrixValue.GetLength(1);
+                Matrix result = new Matrix(xLength, yLength);
+                for (int i = 0; i < xLength; i++)
+                {
+                    for (int j = 0; j < yLength; j++)
+                    {
+                        result[i, j] = a[i, j] * n;
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            if (a != null || b != null)
+            {
+                if (a.MatrixValue.GetLength(0) == b.MatrixValue.GetLength(0)
+                    && a.MatrixValue.GetLength(1) == b.MatrixValue.GetLength(1))
+                {
+                    int xLength = a.MatrixValue.GetLength(0);
+                    int yLength = a.MatrixValue.GetLength(1);
+                    Matrix sum = new Matrix(xLength, yLength);
+                    for (int i = 0; i < xLength; i++)
+                    {
+                        for (int j = 0; j < yLength; j++)
+                        {
+                            sum[i, j] = a[i, j] + b[i, j];
+                        }
+                    }
+
+                    return sum;
+                }
+                else
+                {
+                    throw new ArgumentException("Both matrices must be the same length and width");
+                }
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            if (a != null || b != null)
+            {
+                return new Matrix(a + (b * -1));
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a != null || b != null)
+            {
+                int aXLength = a.MatrixValue.GetLength(0);
+                int aYLength = a.MatrixValue.GetLength(1);
+                int bXLength = b.MatrixValue.GetLength(0);
+                int bYLength = b.MatrixValue.GetLength(1);
+
+                if (aYLength == bXLength)
+                {
+                    Matrix result = new Matrix(aXLength, bYLength);
+                    for (int i = 0; i < aXLength; i++)
+                    {
+                        for (int j = 0; j < bYLength; j++)
+                        {
+                            int intermediateResult = 0;
+                            for (int k = 0; k < aYLength; k++)
+                            {
+                                intermediateResult += a[i, k] * b[k, j];
+                            }
+
+                            result[i, j] = intermediateResult;
+                        }
+                    }
+
+                    return result;
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        "The number of columns in the first matrix must be equals to the number of rows in second one");
+                }
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static Matrix TransposeMatrix(Matrix matrix)
+        {
+            if (matrix != null)
+            {
+                int x = matrix.MatrixValue.GetLength(0);
+                int y = matrix.MatrixValue.GetLength(1);
+                Matrix result = new Matrix(x, y);
+
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        result[i, j] = matrix[j, i];
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            string matrixInString = "";
+
+            for (int i = 0; i < MatrixValue.GetLength(0); i++)
+            {
+                for (int j = 0; j < MatrixValue.GetLength(1); j++)
+                {
+                    matrixInString += this[i, j] + " ";
+                }
+
+                matrixInString += "\r\n";
+            }
+
+            Console.WriteLine(matrixInString);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || GetType() != obj.GetType() )
+            {
+                return false;
+            }
+            else
+            {
+                Matrix matrix = (Matrix) obj;
+                if (matrix.MatrixValue.GetLength(0) != MatrixValue.GetLength(0) 
+                    || matrix.MatrixValue.GetLength(1) != MatrixValue.GetLength(1))
+                {
+                    return false;
+                }
+                for (int i = 0; i < matrix.MatrixValue.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.MatrixValue.GetLength(1); j++)
+                    {
+                        if (matrix[i, j] != MatrixValue[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public Matrix Copy()
+        {
+            return new Matrix(this);
+        }
+    }
+}
---
+++ Matrix/Polynomial.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice1
+{
+    public class Polynomial
+    {
+        public Dictionary<int, int> IntPolynomial { get; private set; }
+
+        public Polynomial(int[] powers, int[] coefficients)
+        {
+            if (powers == null || coefficients == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else if (powers.Length != coefficients.Length)
+            {
+                throw new ArgumentException("Length of powers array must be equals to length of coefficients array");
+            }
+            else
+            {
+                IntPolynomial = new Dictionary<int, int>();
+                
+                for (int i = 0; i < powers.Length; i++)
+                {
+                    try
+                    {
+                        IntPolynomial.Add(powers[i], coefficients[i]);
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine($"An element with power {powers[i]} " +
+                                          $"already exists. " +
+                                          $"Note that powers must be unique");
+
+                        throw;
+                    }
+                }
+            }
+        }
+        
+
+        public Polynomial(Polynomial polynomial)
+        {
+            if (polynomial == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                IntPolynomial = polynomial.IntPolynomial;
+            }
+        }
+
+        public Polynomial()
+        {
+            IntPolynomial = new Dictionary<int, int>();
+        }
+
+        public static Polynomial operator +(Polynomial polynomial, int n)
+        {
+            if (polynomial != null)
+            {
+                Polynomial result = new Polynomial();
+                foreach (var kvp in polynomial.IntPolynomial)
+                {
+                    result.IntPolynomial.Add(kvp.Key, kvp.Value + n);
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static Polynomial operator -(Polynomial polynomial, int n)
+        {
+            return new Polynomial(polynomial + n*-1);
+        }
+
+        public static Polynomial operator *(Polynomial polynomial, int n)
+        {
+            if (polynomial != null)
+            {
+                Polynomial result = new Polynomial();
+                foreach (var kvp in polynomial.IntPolynomial)
+                {
+                    result.IntPolynomial.Add(kvp.Key, kvp.Value * n);
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public static Polynomial operator *(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            if (polynomial1 != null || polynomial2 != null)
+            {
+                Polynomial result = new Polynomial();
+
+                foreach (var kvp1 in polynomial1.IntPolynomial)
+                {
+                    foreach (var kvp2 in polynomial2.IntPolynomial)
+                    {
+                        int powSum = kvp1.Key + kvp2.Key;
+                        if (result.IntPolynomial.TryGetValue(powSum, out int value))
+                        {
+                            result.IntPolynomial[powSum] = value + (kvp1.Value * kvp2.Value);
+                        }
+                        else
+                        {
+                            result.IntPolynomial.Add(powSum, kvp1.Value * kvp2.Value);
+                        }
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Polynomial polynomial = (Polynomial) obj;
+            foreach (var kvp1 in IntPolynomial)
+            {
+                foreach (var kvp2 in polynomial.IntPolynomial)
+                {
+                    if (kvp1.Key == kvp2.Key)
+                    {
+                        if (kvp1.Value != kvp2.Value)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public Polynomial Copy()
+        {
+            return new Polynomial(this);
+        }
+    }
+}
